410a936e1147f8eb36c091d4206f8175de464f13
rassek96 <rassek96@gmail.com>
2016-04-20 22:20:59 +0200

    Added choice for ace value in blackjack

--- public/javascripts/blackJack.js
+++ public/javascripts/blackJack.js
@@ -1,12 +1,15 @@
 var textContent = document.querySelector("#textContent");
 var hitBtn = document.querySelector("#hitBtn");
+var stayBtn = document.querySelector("#stayBtn");
 var playerCardBox = document.querySelector("#playerCardBox");
 var playerScore = document.querySelector("#playerScore");
 var buttonsDiv = document.querySelector("#buttons");
 var startGameDiv = document.querySelector("#startGame");
 var startBtn = document.querySelector("#startBtn");
+var aceButtons = document.querySelector("#aceButtons");
 
 var total;
+var dealerTotal;
 var deck = getDeck();
 var shuffledDeck;
 var cardCount;
@@ -17,21 +20,23 @@ startBtn.addEventListener("click", startTheGame);
 function startTheGame() {
   startBtn.removeEventListener("click", startTheGame);
   startGameDiv.style.display = "none";
-  buttonsDiv.style.visibility = "visible";
+  buttonsDiv.style.display = "inline";
+  buttonsDiv.querySelector("#btn").style.display = "inline";
   cardCount = 0;
   total = 0;
   gamesPlayed += 1;
   shuffledDeck = shuffleDeck(deck);
   hit();
-  hitBtn.addEventListener("click", hit);
 }
 
 function hit() {
   var card = shuffledDeck[cardCount];
-
+  //Add cardvalue to total sum
   var cardValue = card.replace(/\D/g,"");
-  total += parseInt(cardValue);
-  playerScore.textContent = total;
+  cardValue = parseInt(cardValue);
+  if(cardValue === 11 || cardValue === 12 || cardValue === 13) {
+    cardValue = 10;
+  }
 
   if(cardCount !== 0 || gamesPlayed > 1) {
     var cardImg = playerCardBox.querySelector("img");
@@ -41,23 +46,57 @@ function hit() {
   cardImg.setAttribute("src", "/images/carddeck/" + card + ".png");
   playerCardBox.appendChild(cardImg);
 
-  cardCount += 1;
 
-  if(total === 21) {
-    alert("winner");
-    hitBtn.removeEventListener("click", hit);
-    startGameDiv.style.display = "inline";
-    buttonsDiv.style.visibility = "hidden";
-    startBtn.addEventListener("click", startTheGame);
-    return;
-  } else if(total > 21) {
-    alert("busted");
-    hitBtn.removeEventListener("click", hit);
-    startGameDiv.style.display = "inline";
-    buttonsDiv.style.visibility = "hidden";
-    startBtn.addEventListener("click", startTheGame);
-    return;
+  if(cardValue === 1) {
+    if(cardCount === 0) {
+      cardValue = 11;
+      total += cardValue;
+      playerScore.textContent = total;
+      checkWin();
+    } else {
+      buttonsDiv.querySelector("#btn").style.display = "none";
+      aceButtons.style.display = "inline";
+      aceButtons.querySelector("#ace1Btn").addEventListener("click", aceWorth1);
+      aceButtons.querySelector("#ace11Btn").addEventListener("click", aceWorth11);
+      function aceWorth1() {
+        aceButtons.querySelector("#ace1Btn").removeEventListener("click", aceWorth1);
+        aceButtons.querySelector("#ace11Btn").removeEventListener("click", aceWorth11);
+        cardValue = 1;
+        total += cardValue;
+        playerScore.textContent = total;
+        buttonsDiv.querySelector("#btn").style.display = "inline";
+        aceButtons.style.display = "none";
+        checkWin();
+      }
+      function aceWorth11() {
+        aceButtons.querySelector("#ace1Btn").removeEventListener("click", aceWorth1);
+        aceButtons.querySelector("#ace11Btn").removeEventListener("click", aceWorth11);
+        cardValue = 11;
+        total += cardValue;
+        playerScore.textContent = total;
+        buttonsDiv.querySelector("#btn").style.display = "inline";
+        aceButtons.style.display = "none";
+        checkWin();
+      }
+    }
+  } else {
+    total += cardValue;
+    playerScore.textContent = total;
+    checkWin();
   }
+
+  cardCount += 1;
+  if(cardCount === 1) {
+    setTimeout(function() {
+      hit();
+      hitBtn.addEventListener("click", hit);
+      stayBtn.addEventListener("click", stay);
+    }, 800);
+  }
+}
+
+function stay() {
+  buttonsDiv.querySelector("#btn").style.display = "none";
 }
 
 function getDeck() {
@@ -87,4 +126,24 @@ function shuffleDeck(deck) {
     shuffleDeck[randomIndex] = temporaryValue;
   }
   return shuffleDeck;
+}
+
+function checkWin() {
+  if(total === 21) {
+    hitBtn.removeEventListener("click", hit);
+    startGameDiv.style.display = "inline";
+    startGameDiv.querySelector("p").textContent = "Winner";
+    buttonsDiv.style.display = "none";
+    buttonsDiv.querySelector("#btn").style.display = "none";
+    startBtn.addEventListener("click", startTheGame);
+    return;
+  } else if(total > 21) {
+    hitBtn.removeEventListener("click", hit);
+    startGameDiv.style.display = "inline";
+    startGameDiv.querySelector("p").textContent = "Busted";
+    buttonsDiv.style.display = "none";
+    buttonsDiv.querySelector("#btn").style.display = "none";
+    startBtn.addEventListener("click", startTheGame);
+    return;
+  }
 }
